f6f019e2c90e298c0eefc6499ee977176f9ca988
Grant Yang <gyang276@berkeley.edu>
2024-03-13 00:31:07 -0700

    s

--- spider_script.js
+++ spider_script.js
@@ -1,18 +1,26 @@
+// Scene
+var scene = new THREE.Scene();
+
+// Camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 5);
+camera.lookAt(0, 0, 0);
+
+// Renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xCCCCCC);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('point-cloud-renderer').appendChild(renderer.domElement);
+
+// Cube
 var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.set(0, 0, 5); // Position the camera away from the origin where the cube is
-camera.lookAt(0, 0, 0); // Ensure the camera is looking at where the cube is placed
-
-
+// Animation
 var animate = function () {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 };
 animate();
-
-scene.add(cube);
-renderer.setClearColor(0xCCCCCC); // Set to a contrasting color
-renderer.setSize(window.innerWidth, window.innerHeight); // Adjust as necessary
