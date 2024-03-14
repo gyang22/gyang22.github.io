1aa9b2b125adbf6e24ba38fbb240e9656205874d
Grant Yang <gyang276@berkeley.edu>
2024-03-13 20:34:31 -0700

    s

--- spider_script.js
+++ spider_script.js
@@ -8,7 +8,6 @@ if (container) {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 0, 5);
-    container.appendChild(renderer.domElement);
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
