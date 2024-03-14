e77d2bd82bf9e0c87175af5dc1057f80b0504507
Grant Yang <gyang276@berkeley.edu>
2024-03-13 20:32:20 -0700

    s

--- spider_script.js
+++ spider_script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.137.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.137.0/examples/jsm/controls/OrbitControls.js';
+// import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.137.0/examples/jsm/controls/OrbitControls.js';
 
 var container = document.getElementById('point-cloud-renderer');
 
