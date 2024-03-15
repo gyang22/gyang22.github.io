0d4810fc6fdb5c1edae8eda1c8b4d20416e54cb3
Grant Yang <gyang276@berkeley.edu>
2024-03-14 20:59:18 -0700

    s

--- spider_script.js
+++ spider_script.js
@@ -6,13 +6,16 @@ var container = document.getElementById('point-cloud-renderer');
 if (container) {
     // Scene
     var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff); // Set background to white
+
     var camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
     
-    // Set the camera position farther away and above the object
-    camera.position.set(0, 5, 10);
-    camera.lookAt(new THREE.Vector3(0, 0, 0)); // Make the camera look at the origin
+    // Set the camera position farther back to fit the object
+    camera.position.set(0, 5, 15); // You may need to adjust this value depending on the size of your object
+    camera.lookAt(new THREE.Vector3(0, 0, 0)); // Camera now looks at the origin
 
-    var renderer = new THREE.WebGLRenderer();
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0xffffff, 1); // Set clear color to white
     renderer.setSize(container.clientWidth, container.clientHeight);
     container.appendChild(renderer.domElement);
 
@@ -20,14 +23,11 @@ if (container) {
     var loader = new PCDLoader();
 
     loader.load('test_web.pcd', function (points) {
+        // Modify the material color of the points
+        points.material.color.setHex(0xff0000); // Change 0xff0000 to any hex color you prefer for the points
+
         scene.add(points);
         points.geometry.center(); // Center the point cloud geometry
-
-        // Rotate the point cloud to the initial desired orientation
-        points.rotation.x = Math.PI / 2;
-        points.rotation.y = 0;
-        points.rotation.z = 0;
-
         animate();
     });
 
