35da111114f4ee5dfcd7bbd7ae6a27bc424c31c4
Grant Yang <gyang276@berkeley.edu>
2024-05-29 10:54:58 -0400

    s

--- spider_script.js
+++ spider_script.js
@@ -4,7 +4,6 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
 import { PCDLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/PCDLoader.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 
-
 var container = document.getElementById('point-cloud-renderer');
 
 if (container) {
@@ -19,6 +18,8 @@ if (container) {
     container.appendChild(renderer.domElement);
 
     var controls = new OrbitControls(camera, renderer.domElement);
+
+    var initialCameraPosition;
 
     var loader = new PCDLoader();
     loader.load('2024-05-25 17-27-41.pcd', function (points) {
@@ -50,54 +51,51 @@ if (container) {
         points.geometry.center(); // Center the geometry
 
         var center = points.geometry.boundingSphere.center;
-        const initialCameraPosition = { x: center.x, y: center.y + 500, z: center.z + 1500 };
-        camera.position.set(center.x, center.y + 500, center.z + 1500); // Adjust the camera position
+        initialCameraPosition = { x: center.x, y: center.y + 500, z: center.z + 1500 };
+        camera.position.set(initialCameraPosition.x, initialCameraPosition.y, initialCameraPosition.z);
         camera.lookAt(center); // Camera now looks at the center of the point cloud
 
         controls.target.set(center.x, center.y, center.z); // Set OrbitControls target
         controls.update();
 
-        animate();
-    });
+        var isUserInteracting = false;
+        var userInteractionTimeout;
 
-    var isUserInteracting = false;
-    var userInteractionTimeout;
+        controls.addEventListener('start', function () {
+            isUserInteracting = true;
+            clearTimeout(userInteractionTimeout);
+        });
 
-    controls.addEventListener('start', function () {
-        isUserInteracting = true;
-        clearTimeout(userInteractionTimeout);
-    });
+        controls.addEventListener('end', function () {
+            userInteractionTimeout = setTimeout(function () {
+                isUserInteracting = false;
+                new TWEEN.Tween(camera.position)
+                    .to(initialCameraPosition, 2000) // Move back to the initial position over 2 seconds
+                    .easing(TWEEN.Easing.Quadratic.Out) // Easing function
+                    .start();
+            }, 3000);
+        });
 
-    controls.addEventListener('end', function () {
-        userInteractionTimeout = setTimeout(function () {
-            isUserInteracting = false;
-            new TWEEN.Tween(camera.position)
-                .to(initialCameraPosition, 2000) // Move back to the initial position over 2 seconds
-                .easing(TWEEN.Easing.Quadratic.Out) // Easing function
-                .start();
-        }, 3000);
-    });
+        var animate = function () {
+            requestAnimationFrame(animate);
 
-    var animate = function () {
-        requestAnimationFrame(animate);
+            if (!isUserInteracting) {
+                scene.rotation.y += 0.01; // Rotate the scene around the Y-axis
+            }
 
-        if (!isUserInteracting) {
-            scene.rotation.y += 0.01; // Rotate the scene around the Y-axis
-        }
+            TWEEN.update(); // Update TWEEN animations
+            controls.update(); // Required if controls.enableDamping or controls.autoRotate are set to true
 
-        TWEEN.update(); // Update TWEEN animations
-        controls.update(); // Required if controls.enableDamping or controls.autoRotate are set to true
+            renderer.render(scene, camera);
+        };
 
-        renderer.render(scene, camera);
-    };
+        animate(); // Start the animation loop
 
-
-    animate(); // Start the animation loop
-
-    window.addEventListener('resize', function () {
-        const aspectRatio = container.clientWidth / container.clientHeight;
-        camera.aspect = aspectRatio;
-        camera.updateProjectionMatrix();
-        renderer.setSize(container.clientWidth, container.clientHeight);
+        window.addEventListener('resize', function () {
+            const aspectRatio = container.clientWidth / container.clientHeight;
+            camera.aspect = aspectRatio;
+            camera.updateProjectionMatrix();
+            renderer.setSize(container.clientWidth, container.clientHeight);
+        });
     });
 }
